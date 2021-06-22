ffe8926cfa1be150be08625b4803477a99f0ca71
Nilkumar Patel <nsp4898@gmail.com>
2021-06-22 16:56:55 -0400

    Completed the equations

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -22,6 +22,7 @@ namespace RatingAdjustment.Services
         void SetPercentPositive(double stars)
         {
             // TODO: Implement this!
+            _percent_positive = (stars * 20) / 100;
         }
 
         /**
@@ -29,7 +30,7 @@ namespace RatingAdjustment.Services
          */
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
+            _q = Z * Math.Sqrt((_percent_positive * (1 - _percent_positive) + Math.Pow(Z, 2) / (4 * number_of_ratings)) / number_of_ratings);
         }
 
         /** Adjusted lower bound
@@ -40,7 +41,10 @@ namespace RatingAdjustment.Services
          */
         public double Adjust(double stars, double number_of_ratings) {
             // TODO: Implement this!
-            return 0.0;
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+            double lower_bound = (_percent_positive + (Math.Pow(Z, 2) / (2 * number_of_ratings)) - _q) / (1 + (Math.Pow(Z, 2) / number_of_ratings)) * MAX_STARS;
+            return lower_bound <= stars ? lower_bound : stars;
         }
     }
 }
